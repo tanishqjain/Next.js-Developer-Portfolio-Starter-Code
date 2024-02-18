235e8314544c92164133fd9506ca756e5c785d08
Tanishq <tanishhhh38@gmail.com>
2024-02-18 11:28:14 +0530

    theme colour code updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} font-mont`}>
+      <main className={`${montserrat.variable} font-mont bg-light width-full min-h-screen`}>
         <Component {...pageProps} />
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
-const {fontFamily} = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
-        mont: ['var(--font-mont)', ...fontFamily.sans]
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
     },
+    colors: {
+      dark: "#1b1b1b",
+      light: "#f5f5f5",
+      primary: "#B63E96", // 240,86,199
+      primaryDark: "#58E6D9", // 80,230,217
+    },
   },
-  plugins: [
-
-  ],
-}
-
+  plugins: [],
+};
